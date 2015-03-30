d3616cb4f81af8baea49028d9b00f6ef9a2c701f
Gabriele Benvenuti <benvenutigab@gmail.com>
2015-03-30 11:21:16 +0200

    Begin GUI design

---
+++ DataPlotter/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+
+namespace DataPlotter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddChartToMainWindow();
+        }
+
+        private void AddChartToMainWindow()
+        {
+            Chart neuralNetErrorChart = new Chart();
+
+            //Series is where the points are "stored"
+            Series netErrorThroughEpochs = new Series();
+            netErrorThroughEpochs.ChartType = SeriesChartType.Line;
+            netErrorThroughEpochs.Color = Color.DodgerBlue;
+
+            //Represent the drawing area
+            ChartArea chartArea = new ChartArea();
+            chartArea.AxisX.Title = "Epoch";
+            chartArea.AxisY.Title = "Net Error (MSE)";
+
+            neuralNetErrorChart.ChartAreas.Add(chartArea);
+            neuralNetErrorChart.Series.Add(netErrorThroughEpochs);
+
+            winformhost.Child = neuralNetErrorChart;
+        }
+
+    }
+}
